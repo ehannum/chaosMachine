c3fb82694e4e0fe2ce8b792099dd378b39a82d6f
Eric <ehannum@gmail.com>
2016-06-02 14:07:40 -0700

    chaos machine now plays random sounds

--- src/directives/mixer.js
+++ src/directives/mixer.js
@@ -7,8 +7,22 @@ chaos.directive('mixer', [function () {
       name: '=',
       sounds: '='
     },
-    controller: ['$scope', function ($scope) {
+    controller: ['$scope', '$timeout', function ($scope, $timeout) {
+      $scope.volume = 100;
+      $scope.delay = 50;
 
+      var playSound = function () {
+        var randomSoundFile = $scope.sounds[Math.floor(Math.random()*$scope.sounds.length)];
+        var audioUrl = 'sounds/' + $scope.name + '/' + randomSoundFile
+        var sound = new Audio(audioUrl);
+        sound.volume = $scope.volume/100;
+        sound.play();
+
+        var randomDelay = Math.random() * $scope.delay * 100 + 1000;
+        $timeout(playSound, randomDelay);
+      }
+
+      playSound();
     }]
   }
 }]);
